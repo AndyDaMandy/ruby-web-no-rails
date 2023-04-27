f936ca632db3d6aaa263546a7982855706db3e17
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-04-27 09:58:54 -0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,11 @@
+class App
+    def call(env)
+      headers = {
+        'Content-Type' => 'text/html'
+      }
+      
+      response_html = File.read 'views/index.html'
+      
+      [200, headers, [response_html]]
+    end
+  end
